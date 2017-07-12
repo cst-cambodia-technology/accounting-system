c83471095667116d0852f0bb0dd43370500814bd
Chomnan Im <chomnan.im@yahoo.com>
2017-07-12 11:05:54 +0700

    commit app source code

---
+++ source-code/accounting-system-app/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AccountingSystemAppPage } from './app.po';
+
+describe('accounting-system-app App', () => {
+  let page: AccountingSystemAppPage;
+
+  beforeEach(() => {
+    page = new AccountingSystemAppPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
